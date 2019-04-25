7382cd4093b1393b2e91f0cd00ee20cb119dc13b
Muhammad Haziq <muhammadhaziq341@gmail.com>
2019-04-26 00:11:27 +0500

    start use context api with useState

---
+++ src/context/AuthContext.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Intial_State = {
+  auth: false
+};
+// const AuthContext = () => {
+export default React.createContext({ Intial_State });
+// };
+
+// export default AuthContext;
--- src/route/Routes.js
+++ src/route/Routes.js
@@ -1,19 +1,24 @@
 import React from "react";
 import MainLayout from "../components/MainLayout.js";
+import LoginForm from "../container/user/LoginForm.js";
 import { BrowserRouter, Route, Redirect } from "react-router-dom";
-
+import AuthContext from "../context/AuthContext.js";
 const Routes = props => {
   return (
     <React.Fragment>
       <BrowserRouter>
-        <MainLayout>
-          <Route>
-            <Route
-              path="/"
-              component={() => <Redirect to=<div>Hello</div> />}
-            />
-          </Route>
-        </MainLayout>
+        <AuthContext.Provider>
+          <MainLayout>
+            <Route>
+              <Route
+                exact
+                path="/"
+                component={() => <Redirect to="/login" />}
+              />
+              <Route exact path="/login" component={LoginForm} />
+            </Route>
+          </MainLayout>
+        </AuthContext.Provider>
       </BrowserRouter>
     </React.Fragment>
   );
