d781a229bc0558c3df6e4e92b781f18d0ee89f5f
Muhammad Haziq <muhammadhaziq341@gmail.com>
2019-08-27 23:45:51 +0500

    changes in message

--- src/container/messages/Message.js
+++ src/container/messages/Message.js
@@ -1,13 +1,19 @@
 import React, { useEffect, useState } from "react";
 import clsx from "clsx";
-import { makeStyles } from "@material-ui/core/styles";
+import { makeStyles, fade } from "@material-ui/core/styles";
 import MenuItem from "@material-ui/core/MenuItem";
 import TextField from "@material-ui/core/TextField";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Box from "@material-ui/core/Box";
+import Paper from "@material-ui/core/Paper";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Avatar from "@material-ui/core/Avatar";
 import withContext from "../../context/ContextHOC.js";
 import * as ACTIONS from "../../actions/chatAction";
+import { FixedSizeList } from "react-window";
 const useStyles = makeStyles(theme => ({
   textField: {
     marginLeft: theme.spacing(1),
@@ -18,53 +24,77 @@ const useStyles = makeStyles(theme => ({
   },
   container: {
     minHeight: "100%"
+  },
+  root: {
+    flexGrow: 1,
+    overflow: "hidden",
+    padding: theme.spacing(0, 3),
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25)
+    }
+  },
+  paper: {
+    maxWidth: "auto",
+    // margin: `${theme.spacing(8)}px auto`,
+    padding: theme.spacing(2)
   }
 }));
 
 const Message = props => {
   const classes = useStyles();
+  const [state, setMessage] = useState({
+    message: ""
+  });
+
+  const handleOnChangeMessage = e => {
+    const { name, value } = e.target;
+    setMessage({ ...state, [name]: value });
+  };
+
+  const submit = e => {
+    e.preventDefault();
+    const data = {
+      message: state.message
+    };
+    console.log(data);
+  };
   console.log(props);
+  console.log(state.message);
   return (
     <React.Fragment>
-      <Typography paragraph>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus
-        non enim praesent elementum facilisis leo vel. Risus at ultrices mi
-        tempus imperdiet. Semper risus in hendrerit gravida rutrum quisque non
-        tellus. Convallis convallis tellus id interdum velit laoreet id donec
-        ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl
-        suscipit adipiscing bibendum est ultricies integer quis. Cursus euismod
-        quis viverra nibh cras. Metus vulputate eu scelerisque felis imperdiet
-        proin fermentum leo. Mauris commodo quis imperdiet massa tincidunt. Cras
-        tincidunt lobortis feugiat vivamus at augue. At augue eget arcu dictum
-        varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt.
-        Lorem donec massa sapien faucibus et molestie ac.
-      </Typography>
-      <Typography paragraph>
-        Consequat mauris nunc congue nisi vitae suscipit. Fringilla est
-        ullamcorper eget nulla facilisi etiam dignissim diam. Pulvinar elementum
-        integer enim neque volutpat ac tincidunt. Ornare suspendisse sed nisi
-        lacus sed viverra tellus. Purus sit amet volutpat consequat mauris.
-        Elementum eu facilisis sed odio morbi. Euismod lacinia at quis risus sed
-        vulputate odio. Morbi tincidunt ornare massa eget egestas purus viverra
-        accumsan in. In hendrerit gravida rutrum quisque non tellus orci ac.
-        Pellentesque nec nam aliquam sem et tortor. Habitant morbi tristique
-        senectus et. Adipiscing elit duis tristique sollicitudin nibh sit.
-        Ornare aenean euismod elementum nisi quis eleifend. Commodo viverra
-        maecenas accumsan lacus vel facilisis. Nulla posuere sollicitudin
-        aliquam ultrices sagittis orci a. itudin aliquam ultrices sagittis orci
-        a.
-      </Typography>
+      <Grid
+        container
+        direction="column"
+        justify="flex-end"
+        alignItems="flex-end"
+      >
+        <Grid item xs>
+          <List className={classes.root}>
+            <ListItem alignItems="flex-start" button>
+              <ListItemText primary="hello" className={classes.root} />
+            </ListItem>
+          </List>
+        </Grid>
+      </Grid>
       <TextField
         fullWidth
-        id="standard-password-input"
-        label="Password"
+        id="standard-Message-input"
+        label="Message"
         className={classes.textField}
-        type="password"
-        autoComplete="current-password"
+        type="text"
         margin="normal"
+        name="message"
+        onChange={handleOnChangeMessage}
       />
     </React.Fragment>
   );
 };
 export default withContext(Message);
+// <Paper className={classes.paper} alignItems="flex-start">
+//   <Grid container wrap="nowrap" spacing={2}>
+//     <Grid item>
+//       <Avatar>W</Avatar>
+//     </Grid>
+//   </Grid>// </Paper>
